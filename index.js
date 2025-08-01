b4c3d0bfd26fa5e129ca85aeef769e3edbba689f
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-08-01 23:42:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits, ChannelType, PermissionsBitField } = require("discord.js");
-const { TOKEN } = require("./token");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
